0dbfc90c3fcfd3d4f1c7cfa0563306e3d3b1cb1a
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-11-27 18:31:06 +0900

    chore: first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import localFont from "next/font/local";
+
+export const metadata = {
+  title: "독스루",
+  description: "기술문서를 함께 번역해보아요",
+};
+
+export const pretendard = localFont({
+  src: "./font/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko-kr">
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
