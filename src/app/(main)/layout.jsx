4a8eb054028c4d8b5c63f92d661c2aeeaca500b5
jieun318 <jieun031800@naver.com>
2025-12-02 14:31:03 +0900

    chore: initial page setup

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import Header from '@/components/Header';
 
-function MainLayout({ children }) {
-  return <>{children}</>;
+export default function MainLayout({ children }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <main className="flex-1 w-full max-w-7xl mx-auto">{children}</main>
+    </div>
+  );
 }
-
-export default MainLayout;
