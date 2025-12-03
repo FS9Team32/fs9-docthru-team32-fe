c2bb357efc769e067dfc355c0876a57a5599800d
gephigirl <maestra_jazmin@naver.com>
2025-12-03 15:54:34 +0900

    chore: fix duplicate imports and function definitions

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,6 @@ import './globals.css';
 import localFont from 'next/font/local';
 import { ModalProvider } from '@/providers/ModalProvider';
 import { AuthProvider } from '@/providers/AuthProvider';
-import './globals.css';
-import localFont from 'next/font/local';
 
 export const metadata = {
   title: '독스루',
@@ -19,8 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko-kr">
       <body className={`${pretendard.className} antialiased`}>
-        <ModalProvider>{children}</ModalProvider>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </AuthProvider>
       </body>
     </html>
   );
