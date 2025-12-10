dfc44fd833620af510230db61f77e6928f90a26e
gephigirl <maestra_jazmin@naver.com>
2025-12-10 09:47:16 +0900

    fix: remove duplicate header

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,11 +1,3 @@
-import Header from '@/components/Header';
-
 export default function MainLayout({ children }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-
-      <main className="flex-1 w-full max-w-7xl mx-auto">{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
