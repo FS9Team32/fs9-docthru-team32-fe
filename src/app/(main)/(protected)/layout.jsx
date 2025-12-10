226b2d3a7923cf96d46e43d3075ffcffcd9ed379
jyoon00-cloud <ljyoon001261@gmail.com>
2025-12-10 17:49:41 +0900

    fix: remove multi header

--- src/app/(main)/(protected)/layout.jsx
+++ src/app/(main)/(protected)/layout.jsx
@@ -11,7 +11,6 @@ export default async function ProtectedLayout({ children }) {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
       <main className="flex-1 w-full max-w-7xl mx-auto">{children}</main>
     </div>
   );
