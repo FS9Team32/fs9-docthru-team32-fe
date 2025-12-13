805a14e8fb65787a0d5bb3319e23c9be435ccb68
jyoon00-cloud <ljyoon001261@gmail.com>
2025-12-14 00:46:47 +0900

    feat: update render html content

--- src/app/(main)/(protected)/challenge/[id]/[workId]/_component/PostContent.jsx
+++ src/app/(main)/(protected)/challenge/[id]/[workId]/_component/PostContent.jsx
@@ -2,6 +2,11 @@
 
 export default function PostContent({ content }) {
   return (
-    <div className="prose prose-lg max-w-none text-gray-900">{content}</div>
+    <div className="prose prose-lg max-w-none text-gray-900">
+      <div
+        className="prose max-w-none leading-relaxed text-gray-800"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </div>
   );
 }
