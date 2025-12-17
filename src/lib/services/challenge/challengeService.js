b412c422e910c47bafabe20a8e5b1832be771d5e
gephigirl <maestra_jazmin@naver.com>
2025-12-17 16:23:00 +0900

    feat: admin edit challenge form

--- src/lib/services/challenge/challengeService.js
+++ src/lib/services/challenge/challengeService.js
@@ -3,6 +3,7 @@ import { defaultFetch, tokenFetch } from '@/lib/utils/fetchClient';
 export const challengeService = {
   getList: () => defaultFetch('/challenges'),
   getMyChallenges: () => tokenFetch('/users/me/challenge'),
+  getById: (challengeId) => tokenFetch(`/challenges/${challengeId}`),
   delete: (challengeId, adminFeedback) =>
     tokenFetch(`/challenges/${challengeId}`, {
       method: 'DELETE',
